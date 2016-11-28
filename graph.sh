4e605f86094fcc5c8eca3fdf301732e886437f25
Maciej Stanek <makaron28@gmail.com>
2016-11-28 16:32:03 +0000

    graph.sh basic loop

--- graph.sh
+++ graph.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-echo "stub"
+dirIn=dat
+dirGpi=gpi
+dirOut=graph
+
+if [ ! -d ./$dirIn ]; then
+	echo "Directory ./$dirIn does not exist. Aborting"
+	exit
+fi
+
+mkdir -p ./$dirGpi
+mkdir -p ./$dirOut
+rm -rf ./$dirGpi/*
+rm -rf ./$dirOut/*
+
+fileList=(`ls -1 ./$dirIn`)
+for f in "${fileList[@]}" ; do
+	printf "\033[36m%s\033[0m\n" $f
+	sleep 0.5
+done
+
