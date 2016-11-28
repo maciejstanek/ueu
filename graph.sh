72144ebad0037d6e3fff9ab872965188961a29af
Maciej Stanek <makaron28@gmail.com>
2016-11-28 17:26:01 +0000

    added gridlines

--- graph.sh
+++ graph.sh
@@ -23,6 +23,8 @@ for f in "${fileList[@]}" ; do
 	echo "set logscale x" >> ./$dirGpi/${name}_gain.gpi
 	echo "set xlabel 'Czestotliwosc [Hz]'" >> ./$dirGpi/${name}_gain.gpi
 	echo "set ylabel 'Wzmocnienie [dB]'" >> ./$dirGpi/${name}_gain.gpi
+	echo "set grid ytics lc rgb '#bbbbbb' lw 1 lt 0" >> ./$dirGpi/${name}_gain.gpi
+	echo "set grid xtics lc rgb '#bbbbbb' lw 1 lt 0" >> ./$dirGpi/${name}_gain.gpi
 	echo "set terminal png size 1000,500" >> ./$dirGpi/${name}_gain.gpi
 	echo "set output './$dirOut/${name}_gain.png'" >> ./$dirGpi/${name}_gain.gpi
 	echo "plot './$dirIn/${name}.dat' using 1:2 smooth csplines notitle, '' using 1:2 with points pointtype 7 notitle" >> ./$dirGpi/${name}_gain.gpi
@@ -32,6 +34,8 @@ for f in "${fileList[@]}" ; do
 	echo "set logscale x" >> ./$dirGpi/${name}_phase.gpi
 	echo "set xlabel 'Czestotliwosc [Hz]'" >> ./$dirGpi/${name}_phase.gpi
 	echo "set ylabel 'Faza [stopnie]'" >> ./$dirGpi/${name}_phase.gpi
+	echo "set grid ytics lc rgb '#bbbbbb' lw 1 lt 0" >> ./$dirGpi/${name}_phase.gpi
+	echo "set grid xtics lc rgb '#bbbbbb' lw 1 lt 0" >> ./$dirGpi/${name}_phase.gpi
 	echo "set terminal png size 1000,500" >> ./$dirGpi/${name}_phase.gpi
 	echo "set output './$dirOut/${name}_phase.png'" >> ./$dirGpi/${name}_phase.gpi
 	echo "plot './$dirIn/${name}.dat' using 1:3 smooth csplines notitle, '' using 1:3 with points pointtype 7 notitle" >> ./$dirGpi/${name}_phase.gpi
