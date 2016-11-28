4b93682e93fa3b1da79c162aaf9199acd6734b03
Maciej Stanek <makaron28@gmail.com>
2016-11-28 17:13:34 +0000

    graphing tool works

--- graph.sh
+++ graph.sh
@@ -18,12 +18,24 @@ fileList=(`ls -1 ./$dirIn`)
 for f in "${fileList[@]}" ; do
 	printf "\033[36m%s\033[0m\n" $f
 	name=${f%%.*}
+	
+	# Gain characteristics
+	echo "set logscale x" >> ./$dirGpi/${name}_gain.gpi
+	echo "set xlabel 'Czestotliwosc [Hz]'" >> ./$dirGpi/${name}_gain.gpi
+	echo "set ylabel 'Wzmocnienie [dB]'" >> ./$dirGpi/${name}_gain.gpi
+	echo "set terminal png size 1000,500" >> ./$dirGpi/${name}_gain.gpi
+	echo "set output './$dirOut/${name}_gain.png'" >> ./$dirGpi/${name}_gain.gpi
+	echo "plot './$dirIn/${name}.dat' using 1:2 smooth csplines notitle, '' using 1:2 with points pointtype 7 notitle" >> ./$dirGpi/${name}_gain.gpi
+	gnuplot ./$dirGpi/${name}_gain.gpi
 
-	echo "gnuplot> set logscale x" >> ./$dirGpi/$name.gpi
-	echo "gnuplot> set xlabel 'Czestotliwosc [Hz]'" >> ./$dirGpi/$name.gpi
-	echo "gnuplot> set ylabel 'Wzmocnienie [dB]'" >> ./$dirGpi/$name.gpi
-	echo "gnuplot> plot 'lo.dat' using 1:2 smooth csplines notitle, '' using 1:2 with points pointtype 7 notitle" >> ./$dirGpi/$name.gpi
+	# Phase characteristics
+	echo "set logscale x" >> ./$dirGpi/${name}_phase.gpi
+	echo "set xlabel 'Czestotliwosc [Hz]'" >> ./$dirGpi/${name}_phase.gpi
+	echo "set ylabel 'Faza [stopnie]'" >> ./$dirGpi/${name}_phase.gpi
+	echo "set terminal png size 1000,500" >> ./$dirGpi/${name}_phase.gpi
+	echo "set output './$dirOut/${name}_phase.png'" >> ./$dirGpi/${name}_phase.gpi
+	echo "plot './$dirIn/${name}.dat' using 1:3 smooth csplines notitle, '' using 1:3 with points pointtype 7 notitle" >> ./$dirGpi/${name}_phase.gpi
+	gnuplot ./$dirGpi/${name}_phase.gpi
 
-	sleep 0.1
 done
 
