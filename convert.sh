0bfeb9ce8d705c124571deff53bb8e95ba2c76e6
maciej-stanek <makaron28@gmail.com>
2017-06-06 12:17:04 +0200

    why I did not commited it?

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dirIn=img
+dirIn=img_2
 dirOut=img_converted
 
 if [ ! -d ./$dirIn ]; then
@@ -18,9 +18,11 @@ for f in "${fileList[@]}" ; do
 	printf "\033[37;46;1m% 4d%%\033[0m \033[36m%s\033[0m\n" $(($fileCount*100/${#fileList[@]})) $f
 	
 	convert ./$dirIn/$f\
-		-crop 1740x890+110+85\
+		-crop 415x390+20+81\
 		-negate\
-		-level 0,35%\
+		-trim\
+		+repage\
 		./$dirOut/$f
+		#-level 0,35%\
 done
 
